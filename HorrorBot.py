d110ed386facab384a6b5ba6892c303aa769ba18
Connor <connorskastner@gmail.com>
2022-12-12 16:17:32 -0600

    Setting up

---
+++ HorrorBot.py
@@ -0,0 +1,24 @@
+### Imports ###
+
+### Start Log ###
+
+### Pull Information From keys.json ###
+# Additionally check if values are empty
+
+### Prevent Repeat Winner ###
+
+### Retrieve Last Post ###
+
+### Scrape & Parse Comments ###
+
+### Log Comments ###
+
+### Choose Winner ###
+
+### Log Winner ###
+
+### Generate Image ###
+
+### Prepare Post ###
+
+### Post Image ###
